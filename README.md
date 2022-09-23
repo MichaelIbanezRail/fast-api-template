715b5cbadddca26a42f1ef2759907cfd177df252
MichaelIbanezRail <63530630+MichaelIbanezRail@users.noreply.github.com>
2022-09-23 00:09:09 -0500

    Added PG Admin

--- README.md
+++ README.md
@@ -20,3 +20,16 @@ Build the images and run the containers:
 ```sh
 $ docker-compose -f docker-compose.prod.yml up -d --build
 ```
+
+
+### Access PG Admin
+
+Access the PG Admin dashboard:
+
+1. [http://localhost:15432/](http://localhost:15432/)
+
+Login using the email and password specified on the docker-compose file.
+
+To connect to the database register a new server.
+
+The host will be the name given to the DB docker container on the docker-compose file. Use the port, user, password, and db specified for the Posgress.
--- app/db.py
+++ app/db.py
@@ -23,35 +23,5 @@ class User(ormar.Model):
     email: str = ormar.String(max_length=128, unique=True, nullable=False)
     active: bool = ormar.Boolean(default=True, nullable=False)
 
-
-class Locations(ormar.Model):
-    class Meta(BaseMeta):
-        tablename = "locations"
-
-    id: int = ormar.Integer(primary_key=True)
-    userId: str = ormar.Integer(unique=False, nullable=False)
-    unixtime: float = ormar.Float(unique=False, nullable=False)
-    lat: float = ormar.Float(unique=False, nullable=False)
-    lon: float = ormar.Float(unique=False, nullable=False)
-    speed: float = ormar.Float(unique=False, nullable=False)
-
-
-class ContainerRecord(ormar.Model):
-    class Meta(BaseMeta):
-        tablename = "container_records"
-
-    id: int = ormar.Integer(primary_key=True)
-    userId: str = ormar.Integer(unique=False, nullable=False)
-    unixtime: float = ormar.Float(unique=False, nullable=False)
-    filename: str = ormar.String(max_length=128, unique=False, nullable=False)
-    imageid: str = ormar.String(max_length=128, unique=False, nullable=False)
-    container_id: str = ormar.String(max_length=128, unique=False, nullable=False)
-    container_numbers: int = ormar.Integer(unique=False, nullable=False)
-    container_check_digit: int = ormar.Integer(unique=False, nullable=False)
-    lat: float = ormar.Float(unique=False, nullable=False)
-    lon: float = ormar.Float(unique=False, nullable=False)
-    speed: float = ormar.Float(unique=False, nullable=False)
-
-
 engine = sqlalchemy.create_engine(settings.db_url)
 metadata.create_all(engine)
